5c904f27c5c7e47eaee38178c5ec6dc1b21782fb
Rasmus Lindahl <rasmus.lindahl@se.abb.com>
2024-06-26 20:57:06 +0200

    Try other

--- scripts/initDatabase.js
+++ scripts/initDatabase.js
@@ -11,17 +11,7 @@ const client = new faunadb.Client({ secret: secretKey });
 async function setupFaunaDB() {
   try {
     // Create a collection
-    await client.query(q.CreateCollection({ name: 'users' }));
-
-    // Create an index
-    await client.query(
-      q.CreateIndex({
-        name: 'users_by_email',
-        source: q.Collection('users'),
-        terms: [{ field: ['data', 'email'] }],
-        unique: true
-      })
-    );
+    await client.query(q.CreateCollection({ name: 'users1' }));
 
     console.log("Database setup complete!");
   } catch (error) {
