1948c6058bcfd2fb9fe6eb12e2a924518a7ad1cb
Rasmus Lindahl <rasmus.lindahl@se.abb.com>
2024-06-26 21:04:11 +0200

    error handling

--- scripts/initDatabase.js
+++ scripts/initDatabase.js
@@ -2,6 +2,12 @@ const faunadb = require('faunadb'),
       q = faunadb.query;
 
 const secretKey = process.env.FAUNADB_SERVER_SECRET;
+if (!secretKey) {
+    return {
+        statusCode: 500,
+        body: JSON.stringify({ error: "Oops! We couldn't find the database key. Please contact support." })
+    };
+}
 
 // Print the key to the console
 console.log("Using FaunaDB Secret:", secretKey);
