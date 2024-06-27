6ce8ba4a0889a1df8ebbe153eb05c63e46807db0
Rasmus Lindahl <rasmus.lindahl@se.abb.com>
2024-06-27 11:26:13 +0200

    Fix create user

--- functions/create-user.js
+++ functions/create-user.js
@@ -1,36 +1,39 @@
-const { getFaunaClient, handleError, q } = require('../utils/faunaClient');
+const faunadb = require('faunadb');
+const q = faunadb.query;
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
+  const secretKey = process.env.DB_KEY;
+  if (!secretKey) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Oops! We couldn't find the database key. Please contact support." })
+    };
+  }
+
+  const client = new faunadb.Client({ secret: secretKey });
+
   try {
-    const client = getFaunaClient();
-    const { personalId, surveyId } = JSON.parse(event.body);
+    const { personalId } = JSON.parse(event.body);
 
-    // Fetch the user reference by personal ID
-    const userRef = await client.query(
-      q.Select(
-        "ref",
-        q.Get(
-          q.Match(q.Index('users_by_personal_id'), personalId)
-        )
-      )
-    );
-
-    // Create the survey with a reference to the user
     const result = await client.query(
       q.Create(
-        q.Collection('Surveys'),
-        { data: { user_ref: userRef, surveyId } }
+        q.Collection('Users'),
+        { data: { personal_id: personalId } }
       )
     );
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: "Great! We've successfully connected your personal ID to this survey.",
+        message: "User created successfully.",
         id: result.ref.id
       })
     };
   } catch (error) {
-    return handleError(error, "Sorry, we couldn't link your personal ID to the survey. Please try again or contact support if the problem persists.");
+    console.error('Error creating user:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Sorry, we couldn't create the user. Please try again or contact support if the problem persists." })
+    };
   }
 };
